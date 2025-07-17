911542a5ca7f927ae93173fcf1b0f705c3e45548
1000dk <1000daekyeong@gmail.com>
2025-07-18 00:31:48 +0900

    refactor: remove model states

--- gta/states/__init__.py
+++ gta/states/__init__.py
@@ -1,13 +1,7 @@
 """States package for LangGraph agents. Provides input/output schemas and state definitions."""
 
 from gta.states.messages_state import MessagesState
-from gta.states.models.base_llm_state import BaseLLMState
-from gta.states.models.openai_state import OpenAIState
-from gta.states.models.ollama_state import OllamaState
 
 __all__ = [
-    "MessagesState",
-    "BaseLLMState",
-    "OpenAIState",
-    "OllamaState"
+    "MessagesState"
 ] 
--- gta/states/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Model-related state definitions.
-"""
-
-from gta.states.models.base_llm_state import BaseLLMState
-from gta.states.models.openai_state import OpenAIState
-from gta.states.models.ollama_state import OllamaState
-
-__all__ = ["BaseLLMState", "OpenAIState", "OllamaState"] 
